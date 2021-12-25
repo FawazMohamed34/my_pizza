7c84bd2ee39bade951ce413cfb6e5668273035fb
moe fwz <moefwzs@gmail.com>
2021-12-25 04:25:21 +0100

    new pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
-    }
+    },
+        'mediterranea': {
+        'ingredients': {'mozzarella': 1, 'onion': 2, 'ham': 3},
+        'price': 11,
+
 }
